87ac8497a868c705c91fa621c598087ddf645593
DimasSetiawan1 <mdimas.stwn1@gmail.com>
2025-06-10 10:49:50 +0700

    fix: Update backup command schedule to run daily at midnight

--- routes/console.php
+++ routes/console.php
@@ -11,4 +11,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 
-Schedule::command("backup:run")->everyMinute();
+Schedule::command("backup:run")->dailyAt('00:00');
