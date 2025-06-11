35e238f3e0aff5e6c1b33fb2a07e86e388387223
DimasSetiawan1 <mdimas.stwn1@gmail.com>
2025-06-11 10:20:24 +0700

    fix: Remove unnecessary lowercase transformation from login throttle key

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -52,7 +52,7 @@ class FortifyServiceProvider extends ServiceProvider
 
 
         RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())) . '|' . $request->ip());
+            $throttleKey = Str::transliterate($request->input(Fortify::username()) . '|' . $request->ip());
 
             return Limit::perMinute(5)->by($throttleKey);
         });
